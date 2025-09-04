1c072f26b27fc14f27fbdcea15bc96eb487ede6e
haris <mhariss386@gmail.com>
2025-09-04 18:57:07 +0500

    update commit

--- JobApplication/jobApp/src/script/context.jsx
+++ JobApplication/jobApp/src/script/context.jsx
@@ -12,11 +12,25 @@ const initialJobs = [
       "A backend Python developer architects and builds the server-side logic, APIs, and data systems that power applications. They ensure scalability, performance, and reliability to handle growing user loads and data. Their code forms the core engine that the front-end and other services rely upon.",
   },
   {
-    title: "Backend Python Developer",
+    title: "Backend Node js Developer",
+    location: "On-site",
+    type: "Part-time",
+    summary:
+      "A backend  developer  builds the server-side logic, APIs, and data systems that power applications. They ensure scalability, performance, and reliability to handle growing user loads and data. Their code forms the core engine that the front-end and other services rely upon.",
+  },
+  {
+    title: "UI designer",
+    location: "Remote",
+    type: "Part-time",
+    summary:
+      "UI Designer role focused on creating the visual and interactive elements of digital products. The position involves designing user interfaces that are both aesthetically pleasing and functionally intuitive to ensure a superior user experience.",
+  },
+  {
+    title: "Data Engineer",
     location: "Remote",
     type: "Full-time",
     summary:
-      "A backend Python developer architects and builds the server-side logic, APIs, and data systems that power applications. They ensure scalability, performance, and reliability to handle growing user loads and data. Their code forms the core engine that the front-end and other services rely upon.",
+      "Designs, builds, and maintains scalable data pipelines and infrastructure to support data analytics and machine learning initiatives. Ensures data is accessible, reliable, and efficiently processed for business intelligence.",
   },
 ];
 
